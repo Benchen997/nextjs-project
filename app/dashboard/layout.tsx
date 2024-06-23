c45f905d71e01249c98574d7d5023c7bcf9fc977
Benchen997 <benchen.melbournechen0422@gmail.com>
2024-06-23 19:07:23 +1000

    delete experimental ppr

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-export const experimental_ppr = true;
+//export const experimental_ppr = true;
 import React from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
