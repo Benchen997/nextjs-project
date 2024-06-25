d683c51ddcc5e506f68d36b75fa212d1da40433c
Benchen997 <benchen.melbournechen0422@gmail.com>
2024-06-25 19:36:10 +1000

    add create invoice logic

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,23 +2,18 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
-//import { useDebouncedCallback } from 'use-debounce';
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
-    /*const handleSearch = useDebouncedCallback((term) => {
-        console.log(`Searching... ${term}`);
-        const params = new URLSearchParams(searchParams);
-        params.set('page', '1');
-        if (term) {
-            params.set('query', term);
-        } else {
-            params.delete('query');
-        }
-        replace(`${pathname}?${params.toString()}`);
-    }, 300);*/
+    /*
+    *   1.Capture the user's input.
+        2.Update the URL with the search params.
+        3.Keep the URL in sync with the input field.
+        4.Update the table to reflect the search query.
+    */
     function handleSearch(term: string) {
         console.log(`Searching... ${term}`);
         const params = new URLSearchParams(searchParams);
